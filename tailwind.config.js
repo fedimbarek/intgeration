cea52750850092752045b8f3396651fac327b045
fedimbarek <fedimbarekabidi@gmail.com>
2025-12-19 20:46:17 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./public/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#06BBCC',
+      },
+      fontSize: {
+        title: '22px',
+      },
+    },
+  },
+  plugins: [],
+}
